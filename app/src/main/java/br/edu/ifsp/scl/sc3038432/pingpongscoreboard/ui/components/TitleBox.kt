771c5a9bc176b2d151917a451db69cb4dd5f94dc
hikazudani <lhjundi@proton.me>
2026-08-30 15:30:51 -0300

    feat: adds TitleBox component

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/pingpongscoreboard/ui/components/TitleBox.kt
@@ -0,0 +1,27 @@
+package br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TitleBox(modifier: Modifier = Modifier){
+    Box(modifier = modifier
+        .fillMaxSize()
+        .padding(24.dp)
+    ) {
+        Text(
+            text = "PingPongScoreBoard",
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+    }
+}
