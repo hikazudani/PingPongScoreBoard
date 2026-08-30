61d57790d1e318d45746eed3d26eb20c984b2376
hikazudani <lhjundi@proton.me>
2026-08-30 15:43:57 -0300

    feat: adds stateful PingPongScoreBoard with remember

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/pingpongscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/pingpongscoreboard/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.components.PointButton
+import br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.components.ResetScoreButton
+import br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.components.Score
+import br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.components.TitleBox
 import br.edu.ifsp.scl.sc3038432.pingpongscoreboard.ui.theme.PingPongScoreBoardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PingPongScoreBoardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PingPongScoreBoard(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PingPongScoreBoard(modifier: Modifier = Modifier) {
+
+    var pointsA by remember { mutableIntStateOf(0) }
+    var pointsB by remember { mutableIntStateOf(0) }
+
+    TitleBox()
+
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically)) {
+
+        Score( pointsPlayer1 = pointsA, pointsPlayer2 = pointsB )
+
+        PointButton( onPointA = {pointsA ++}, onPointB = {pointsB ++})
+
+        ResetScoreButton {
+            pointsA = 0
+            pointsB = 0
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PingPongScoreBoardPreview() {
     PingPongScoreBoardTheme {
-        Greeting("Android")
+        PingPongScoreBoard()
     }
 }
